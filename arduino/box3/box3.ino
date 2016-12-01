044f217ed86b9f8901ba8de9c98043709f21431e
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2016-12-01 20:09:05 +0100

    mqtt in progress

---
+++ arduino/box3/box3.ino
@@ -0,0 +1,100 @@
+#define MQTT_KEEPALIVE 2
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+// libraries
+#include <GSM.h>
+
+// PIN Number
+#define PINNUMBER ""
+
+// APN data
+#define GPRS_APN       "internet" // replace your GPRS APN
+#define GPRS_LOGIN     ""    // replace with your GPRS login
+#define GPRS_PASSWORD  "" // replace with your GPRS password
+
+// MQTT Broker
+#define MQTT_SERVER "test.mosquitto.org"
+//"ParkingInfo.azure-devices.net"
+
+// initialize the library instance
+GSMClient gsmClient;
+GPRS gprs;
+GSM gsmAccess;
+
+PubSubClient client(gsmClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      client.subscribe("/jacek");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  // initialize serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("Starting mqtt client.");
+  // connection state
+  boolean notConnected = true;
+
+  // After starting the modem with GSM.begin()
+  // attach the shield to the GPRS network with the APN, login and password
+  while (notConnected) {
+    if ((gsmAccess.begin(PINNUMBER) == GSM_READY) &
+        (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == GPRS_READY)) {
+      notConnected = false;
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+
+  Serial.println("connecting...");
+
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
+
+  // Allow the hardware to sort itself out
+  delay(1500);
+}
+
+void loop()
+{
+  Serial.println("loop 1");
+  if (!client.connected()) {
+    reconnect();
+  }
+  Serial.println("loop 2");
+  client.loop();
+  Serial.println("loop 3");
+  delay(1000);
+}
